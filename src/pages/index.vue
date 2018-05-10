00af8aac1accd65a2554ec42738c0f02f66d4e76
zhaohuan <690018019@qq.com>
2018-05-10 18:24:18 +0800

    删除多的页面

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,46 +1,45 @@
 <template>
-        <div>
-                <div class="manTitle">
-                        <headBox v-bind:show='show' v-on:changeStatus = "changeSt()"></headBox>
-                        </div>
-                        <div class="mainBox">
-                        <div class="leftPart" v-if='show'>
-                            <globalMenu></globalMenu>
-                        </div>
-                        <div class="rightPart">
-                            <div class="topPart"></div>
-                            <div class="manBox">
-                                <router-view></router-view>
-                            </div>
-                        </div>
-                    </div>
+    <div style="height: 100%;">
+        <div class="manTitle">
+            <headBox v-bind:show='show' v-on:changeStatus = "changeSt()"></headBox>
+            </div>
+            <div class="mainBox">
+            <div class="leftPart" v-if='show'>
+                <globalMenu></globalMenu>
+            </div>
+            <div class="rightPart">
+                <div class="topPart"></div>
+                <div class="manBox">
+                    <router-view></router-view>
+                </div>
+            </div>
         </div>
-
-    </template>
-    <script>
-        import globalMenu from '../components/menu'
-        import headBox from '../components/headBox'
-        export default {
-          name: 'hello',
-          data () {
-            return {
-                  show:true
-            }
-          },
-          components: {
-            globalMenu,
-            headBox
-        },
-          methods: {
-              
-            changeSt:function(){
-                if(this.show==true){
-                    this.show = false
-                }else{
-                    this.show = true
-                }
-            },
+    </div>
+</template>
+<script>
+    import globalMenu from '../components/menu'
+    import headBox from '../components/headBox'
+    export default {
+        name: 'hello',
+        data () {
+        return {
+                show:true
         }
+        },
+        components: {
+        globalMenu,
+        headBox
+    },
+        methods: {
+            
+        changeSt:function(){
+            if(this.show==true){
+                this.show = false
+            }else{
+                this.show = true
+            }
+        },
     }
-    </script>
+}
+</script>
     
